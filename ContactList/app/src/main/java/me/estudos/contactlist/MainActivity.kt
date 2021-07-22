9560ddbfcbf836bee82fee15635d1b979fab37f0
raulhsant <raulhsantana@gmail.com>
2021-07-21 21:45:28 -0300

    Adiciona menus

--- ContactList/app/src/main/java/me/estudos/contactlist/MainActivity.kt
+++ ContactList/app/src/main/java/me/estudos/contactlist/MainActivity.kt
@@ -1,7 +1,14 @@
 package me.estudos.contactlist
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -11,10 +18,27 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.drawer_menu)
 
+        initDrawer()
         bindViews()
         updateList()
+    }
+
+    private fun initDrawer() {
+        val drawerLayout = findViewById<View>(R.id.drawer_layout) as DrawerLayout
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        val toggle = ActionBarDrawerToggle(
+            this,
+            drawerLayout,
+            toolbar,
+            R.string.open_drawer,
+            R.string.close_drawer
+        )
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
     }
 
     private fun bindViews() {
@@ -29,5 +53,28 @@ class MainActivity : AppCompatActivity() {
                 Contact("Talita Santos", "(00) 0000-0000", "img.png")
             )
         )
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.item_menu_1 -> {
+                showToast("Click no item de menu 1")
+                return true
+            }
+            R.id.item_menu_2 -> {
+                showToast("Click no item de menu 2")
+                return true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
